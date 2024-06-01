788634bdab91e0e0c0344628bb21b446c5f51741
omonterroso-2022141 <omonterroso-2022141@kinal.edu.gt>
2024-06-01 02:33:44 -0600

    Revision y actualizacion de la vista chat

---
+++ chatRevisado/chat/lib/domain/entities/message.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+enum FromWho { me, hers }
+
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromWho;
+  final Color color;
+
+  Message({required this.text, this.imageUrl, required this.fromWho, required this.color});
+}
